1c5ada4860228b12d2e327e70aeeb19e73e6817e
ubriela <hto@usc.edu>
2015-07-21 02:12:57 -0700

    update test file

--- test/geoq_test.py
+++ test/geoq_test.py
@@ -2,7 +2,7 @@ __author__ = 'ubriela'
 
 import SwaggerMediaq as sm
 
-url = "http://local.eclipse.org/MediaQ_MVC_V3/api"
+url = "http://mediaq.usc.edu/MediaQ_MVC_V3/api"
 
 # Create geoq client api
 geoq = sm.GeoqApi(sm.ApiClient(url))
@@ -20,9 +20,9 @@ geoq = sm.GeoqApi(sm.ApiClient(url, "X-API-KEY", "8b51UFM2SlBltx3s6864eUO1zSoefe
 # Returns a set of video locations
 geoq.rectangle_query(swlat=34.019972,swlng=-118.291588,nelat=34.021111,nelng=-118.287125)
 
-# Returns a set of video locations that are captured within a time interval (from -> to)
+# Returns a set of video locations that are captured within a time interval (startdate -> enddate)
 geoq.rectangle_query(swlat=34.019972,swlng=-118.291588,nelat=34.021111,nelng=-118.287125,startdate="2015-01-01 00:00:00",enddate="2016-01-01 00:00:00")
-# http://mediaq.usc.edu/MediaQ_MVC_V3/api/geoq/rectangle_query?swlng=-118.291588&nelat=34.021111&nelng=-118.287125&from=2015-01-01 2000:00:00&to=2016-01-01 00:00:00swlat=34.019972&X-API-KEY=REAL_KEY
+# http://mediaq.usc.edu/MediaQ_MVC_V3/api/geoq/rectangle_query?swlng=-118.291588&nelat=34.021111&nelng=-118.287125&startdate=2015-01-01 2000:00:00&enddate=2016-01-01 00:00:00swlat=34.019972&X-API-KEY=REAL_KEY
 
 # Returns a set of video frames
 geoq.video_metadata("d0a0163e5d852e70f87bdc8718166e42989306a5")
