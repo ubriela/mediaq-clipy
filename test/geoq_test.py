e4338dcc9b11b87e461892ede376ffd31abf1233
Hien To <ubriela@users.noreply.github.com>
2015-07-21 01:08:25 -0700

    Update geoq_test.py

--- test/geoq_test.py
+++ test/geoq_test.py
@@ -18,5 +18,10 @@ geoq = sm.GeoqApi(sm.ApiClient("http://mediaq.usc.edu/MediaQ_MVC_V3/api", "X-API
 # Returns a set of video locations
 print geoq.rectangle_query(swlat=34.019972,swlng=-118.291588,nelat=34.021111,nelng=-118.287125)
 
+# Returns a set of video locations that are captured within a time interval (from -> to)
+print geoq.rectangle_query(from='2015-01-01 2000:00:00', to='2016-01-01 00:00:00'&swlat=34.019972,swlng=-118.291588,nelat=34.021111,nelng=-118.287125)
+# http://mediaq.usc.edu/MediaQ_MVC_V3/api/geoq/rectangle_query?from=2015-01-01 2000:00:00&to=2016-01-01 00:00:00swlat=34.019972&swlng=-118.291588&nelat=34.021111&nelng=-118.287125&X-API-KEY=REAL_KEY
+
 # Returns a set of video frames
-print geoq.video_metadata("jca1ptaaiz83_2015_1_16_Videotake_1421449431727.mp4")
+print geoq.video_metadata("d0a0163e5d852e70f87bdc8718166e42989306a5")
+# http://mediaq.usc.edu/MediaQ_MVC_V3/api/geoq/video_metadata?vid=d0a0163e5d852e70f87bdc8718166e42989306a5&X-API-KEY=REAL_KEY
